2aa58cdf8c5a2fa04316d8c00988f3db3ea26d12
Joseph Levine <joseph.levine@senate.mn>
2021-01-18 14:56:14 -0600

    chapter 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo servo;
+
+int const SERVO_PIN = 9;
+int const POTENTIMETER = A0;
+int const MOVE_DELAY = 15;
+
 void setup() {
-// write your initialization code here
+  servo.attach(SERVO_PIN);
+  Serial.begin(9600);
 }
 
 void loop() {
-// write your code here
+  servo.write(map(analogRead(potentimeter), 0, 1023, 0, 179));
+  delay(MOVE_DELAY);
 }
