3fbe0d205b22605a744d79fe16281a9b709b28aa
Joseph Levine <joseph.levine@senate.mn>
2021-02-08 08:05:02 -0600

    chapter-7

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <Arduino.h>
+
+#ifndef PIEZO_PIN
+#define PIEZO_PIN 8
+#endif
+
+const int notes[4] = {262, 294, 330, 349};
 void setup() {
-// write your initialization code here
+  Serial.begin(9600);
 }
 
 void loop() {
-// write your code here
+  int keyVal = analogRead(A0);
+  if (keyVal == 1023) {
+    tone(PIEZO_PIN, notes[0]);
+  } else if (keyVal >= 990 && keyVal <= 1010) {
+    tone(PIEZO_PIN, notes[1]);
+  } else if (keyVal >= 505 && keyVal < 515) {
+    tone(PIEZO_PIN, notes[2]);
+  } else if (keyVal >= 5 && keyVal <= 10) {
+    tone(PIEZO_PIN, notes[3]);
+  } else {
+    noTone(PIEZO_PIN);
+  }
 }
